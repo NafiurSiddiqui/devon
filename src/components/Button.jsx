dac7cbc3be688338f06beb0446578fc94b8b5c44
Nafi R <saejim91@gmail.com>
2023-03-19 22:16:14 +0600

    button added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 
 /**
  *
- * @param {text:string} children,
- * @param { size:string } type
+ * @param {chilren:string} ,
+ * @param { size:string }
+ * @param { path:string}
  * @returns
  */
-export default function Button({ children, type, path }) {
+export default function Button({ children, size, path }) {
 	//change size based on the type, s/m/l
+	const s = 'px-py-1 rounded-sm border-2 border-slate-700 px-1 py-1.5 text-lg';
 	return (
-		<button type="button">
+		<button type="button" className={s}>
 			<Link href={path}>{children}</Link>
 		</button>
 	);
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '../Button';
 
 export default function Navbar() {
 	return (
 		<nav>
 			<span className="navbar-logo">Devon</span>
+			<Button size={'m'} path={'/login'}>
+				Log in
+			</Button>
 		</nav>
 	);
 }
