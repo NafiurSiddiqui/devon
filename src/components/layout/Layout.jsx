8aec0e43803ce7312a0a0dbf506f2810932c1ef5
Nafi R <saejim91@gmail.com>
2023-03-20 05:44:41 +0600

    navbar inprogress

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
-import SidebarMenu from '../Sidebar/Sidebar';
+import SidebarMenu from '../sidebar/Sidebar';
 
 export default function Layout({ children }) {
 	return (
