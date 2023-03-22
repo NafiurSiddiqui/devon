c64a1528e6b502b7b653527df79ab8ec79c8b8e8
Nafi R <saejim91@gmail.com>
2023-03-22 19:51:51 +0600

    sidebar toggling added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Theme from '../theme';
 
-export default function SidebarMenu() {
+export default function SidebarMenu({ open }) {
+	//You do not need this if you are using Nextra
 	return (
-		<aside>
+		<aside className={`${open ? 'inline-block' : 'hidden'}`}>
 			<ul>
 				<li>Introduction</li>
 			</ul>
