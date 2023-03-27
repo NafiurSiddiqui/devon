ecab87257b5a9af11a9a4f353ba8ae9431007384
Nafi R <saejim91@gmail.com>
2023-03-27 23:28:54 +0600

    [0]

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -52,15 +52,15 @@ export default function HomePage() {
 				</div>
 			</section>
 			{/* SECOND SECTION */}
-			<section>
-				<article className="">
-					<p>
+			<section className="borderTest h-screen py-4">
+				<article className="borderTest my-4 mx-2 pt-4">
+					<p className="mb-4 font-semibold text-gray-700">
 						Signing up lets you track your progress, rewards, follow and
 						interacts with others in the community.
 					</p>
 
-					<div>
-						<hr />
+					<div className="borderTest flex items-center justify-center">
+						<span className=" inline-block  h-1 w-full border-t-2 border-slate-400 "></span>
 						<Button path={'/signup'} type={'fill'}>
 							Sign up
 						</Button>
