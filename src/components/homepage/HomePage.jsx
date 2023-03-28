380525de767d3a30de5527b5d8014ead04de4373
Nafi R <saejim91@gmail.com>
2023-03-29 01:39:10 +0600

    [0]

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
 	return (
 		<>
 			<section className=" relative z-10 h-screen overflow-hidden">
-				<div className="relative flex h-screen flex-col items-center">
+				<div className="relative flex h-screen flex-col items-center ">
 					<Image
 						className="supportForSafari   h-screen brightness-50 contrast-75 tablet-md:object-cover "
 						src={HeroImage}
@@ -18,10 +18,10 @@ export default function HomePage() {
 						priority={true}
 					/>
 					<article
-						className={` absolute top-1/4  px-6  android-md/2:w-[28rem] tablet:left-8 tablet:w-[30rem] tablet-md:w-[36rem] desktop:w-[44rem] desktop-md:left-32  `}
+						className={`borderTest  absolute top-[20%]  px-6  android-md/2:w-[28rem] tablet:left-8 tablet:w-[30rem] tablet-md:w-[36rem] desktop:w-[44rem] desktop-md:left-32  `}
 					>
 						<h1
-							className={`pb-4 text-5xl font-bold text-slate-100  desktop:text-9xl`}
+							className={`pb-4 text-5xl font-bold text-slate-100  desktop:text-5xl`}
 						>
 							Dev up
 						</h1>
@@ -32,7 +32,8 @@ export default function HomePage() {
 					text-white
 					tablet:pr-4
 					tablet:text-lg
-					desktop:text-4xl
+				
+					desktop:text-xl
 					"
 						>
 							Kick start your career with Devon and be a part of massive tech
@@ -50,24 +51,24 @@ export default function HomePage() {
 					</article>
 
 					<div className="absolute -bottom-[9rem] -left-[2rem] h-[14rem] w-[150rem] rotate-[7deg] bg-slate-50   ">
-						<h2 className="laptop:-top-22 tablet-md:4xl relative -top-24 ml-16 h-80 -rotate-[7deg] text-xl  font-semibold text-stone-600  laptop:text-4xl desktop-md:ml-20">
+						<h2 className="laptop:-top-22 relative -top-24 ml-16 h-80 -rotate-[7deg] text-xl font-semibold  text-stone-600 tablet-md:text-2xl  desktop-md:-top-20 desktop-md:ml-44">
 							Should you signup?
 						</h2>
 					</div>
 				</div>
 			</section>
 			{/* SECOND SECTION */}
-			<div className=" mb-8 justify-center tablet:flex tablet-md:h-[60vh]">
+			<div className=" mb-8 justify-center tablet:flex ">
 				<section className="  py-4 px-2 leading-6 android-md/2:px-4  tablet:w-4/5 desktop:w-3/5 ">
-					<div className=" ">
-						<article className="my-10 mx-2 rounded border border-stone-300 p-6   tablet-md:w-4/5 tablet-md:px-6 laptop:w-[50rem] laptop:px-8">
-							<p className="mb-4 font-semibold text-stone-700 tablet:text-xl tablet-md:w-4/5  desktop-md:text-3xl">
+					<div className=" tablet:flex tablet:justify-start">
+						<article className="my-10 mx-2 rounded border border-stone-300 p-6   tablet:max-w-lg tablet-md:px-6 laptop:w-[50rem] laptop:px-8  ">
+							<p className="mb-4 text-sm font-semibold leading-6 text-stone-700 	laptop:text-lg  desktop-md:text-xl">
 								Signing up lets you track your progress, rewards, follow and
 								interacts with others in the community.
 							</p>
 
 							<div className=" flex items-center justify-between">
-								<span className=" inline-block  h-1 w-[55%] border-t-2 border-slate-400 pr-4 tablet-md:w-2/5"></span>
+								<span className=" inline-block  h-1 w-[45%] border-t-2 border-slate-400 pr-4 tablet-md:w-2/5"></span>
 								<Button path={'/signup'} type={'fill'} singleBtn={true}>
 									Sign up
 								</Button>
@@ -76,14 +77,14 @@ export default function HomePage() {
 					</div>
 
 					<div className=" tablet:flex tablet:justify-end">
-						<article className="my-10 mx-2 rounded border border-stone-300 p-6   tablet-md:w-4/5 tablet-md:px-6 laptop:w-[50rem] laptop:px-8">
-							<p className="mb-4 font-semibold text-stone-700 tablet:text-xl tablet-md:w-4/5  desktop-md:text-3xl">
+						<article className="my-10 mx-2 rounded border border-stone-300 p-6 tablet:max-w-lg   tablet-md:px-6 laptop:w-[50rem] laptop:px-8">
+							<p className="mb-4 text-sm font-semibold leading-6 text-stone-700 laptop:text-lg  desktop-md:text-xl">
 								Or you can start learning right away without saving your
 								progress.
 							</p>
 
 							<div className=" flex items-center justify-between">
-								<span className=" inline-block  h-1 w-[55%] border-t-2 border-slate-400 pr-4 tablet-md:w-3/5"></span>
+								<span className=" inline-block  h-1 w-[45%] border-t-2 border-slate-400 pr-4 "></span>
 								<Button path={'/guest'} type={'no-fill'} singleBtn={true}>
 									Guest
 								</Button>
