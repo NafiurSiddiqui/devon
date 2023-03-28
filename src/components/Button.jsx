af3821d91b82a14803eb530767f9f412127547a2
Nafi R <saejim91@gmail.com>
2023-03-28 20:31:04 +0600

    [0]

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,18 +9,14 @@ import React from 'react';
  * @returns
  */
 export default function Button({ children, type, path }) {
-	//change size based on the type, s/m/l
-	const s =
-		'px-py-1 rounded-sm border-2 border-slate-700 px-2 py-1 font-semibold  ';
-
 	const fill =
-		'h-10 w-36 rounded border-2 border-slate-600 bg-slate-600 text-white font-semibold text-sm  text-slate-600 hover:border-0 hover:border-none hover:border-transparent hover:bg-slate-700 hover:text-white tablet-md:w-56 tablet-md:text-lg tablet-md:h-12';
+		'h-10 w-36 rounded border-[3px] border-slate-500 bg-slate-600 text-white font-semibold text-sm  text-slate-600 hover:border-0 hover:border-none hover:border-transparent hover:bg-slate-700 hover:text-white tablet-md:w-56 tablet-md:text-lg tablet-md:h-12 tablet-md:h-[3.2rem] tablet-md:rounded';
 
 	const noFillDark =
-		'h-10 w-36 text-sm rounded-sm border-2 border-slate-500 font-semibold text-slate-300 hover:border-0 hover:border-none hover:border-transparent hover:bg-slate-700 hover:text-white tablet-md:w-56 tablet-md:h-12 ';
+		'h-10 w-36 text-sm rounded-sm border-2 border-slate-400 font-semibold text-slate-300 hover:border-0 hover:border-none hover:border-transparent hover:bg-slate-700 hover:text-white tablet-md:w-56 tablet-md:h-12 tablet-md:text-lg tablet-md:h-[3.2rem] tablet-md:rounded';
 
 	const noFill =
-		'h-10  w-36 text-sm rounded-sm border-2 border-slate-500 font-semibold text-slate-600 hover:border-0 hover:border-none hover:border-transparent hover:bg-slate-700 hover:text-white tablet-md:w-56 tablet-md:h-12';
+		'h-10  w-36 text-sm rounded-sm border-2 border-slate-500 font-semibold text-slate-600 hover:border-0 hover:border-none hover:border-transparent hover:bg-slate-700 hover:text-white tablet-md:w-56 tablet-md:h-12 tablet-md:text-lg tablet-md:h-[3.2rem] tablet-md:rounded';
 
 	return (
 		<button
--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -18,25 +18,28 @@ export default function HomePage() {
 						priority={true}
 					/>
 					<article
-						className={` absolute top-1/4  px-6 android-md:w-[22rem] tablet:left-8 tablet:w-[22rem] tablet-md:w-[22rem]  `}
+						className={` absolute top-1/4  px-6 android-md:w-[22rem] tablet:left-8 tablet:w-[30rem] tablet-md:w-[36rem] desktop:w-[44rem] desktop-md:left-32  `}
 					>
 						<h1
-							className={`sm:text-6xl pb-4 text-4xl font-bold text-slate-100`}
+							className={`pb-4 text-6xl font-bold text-slate-100 tablet-md:text-8xl desktop:text-9xl`}
 						>
 							Dev up
 						</h1>
 						<p
 							className="text-bold
-					 
-					mb-7
+					 my-7
+					text-lg
 					text-white
+					tablet:pr-4
+					tablet:text-2xl
+					desktop:text-4xl
 					"
 						>
 							Kick start your career with Devon and be a part of massive tech
 							industry that always pays.
 						</p>
 
-						<div className=" flex justify-between">
+						<div className=" mt-16 flex justify-between tablet:w-[22rem] tablet-md:w-[32rem] ">
 							<Button path={'/signup'} type={'fill'}>
 								Sign up
 							</Button>
@@ -46,25 +49,25 @@ export default function HomePage() {
 						</div>
 					</article>
 
-					<div className="absolute -bottom-[6.438rem] -left-[2rem] h-[12rem] w-[100rem] rotate-[7deg] bg-slate-50  ">
-						<h2 className="relative -top-12 ml-8 -rotate-[7deg] text-2xl font-semibold  text-gray-700">
+					<div className="absolute -bottom-[11rem] -left-[2rem] h-[14rem] w-[150rem] rotate-[7deg] bg-slate-50   ">
+						<h2 className="laptop:-top-22 relative -top-20 ml-16 h-80 -rotate-[7deg] text-2xl font-semibold  text-stone-600 laptop:text-3xl desktop-md:ml-20">
 							Should you signup?
 						</h2>
 					</div>
 				</div>
 			</section>
 			{/* SECOND SECTION */}
-			<div className="h-[60vh] justify-center tablet:flex">
-				<section className="borderTest   py-4 px-2 leading-6 android-md/2:px-4  tablet:w-4/5 desktop:w-3/5 ">
-					<div className="borderTest ">
-						<article className="borderTest my-10 mx-2 pt-4 tablet:w-96 tablet-md:w-4/5 laptop:w-[40rem]">
-							<p className="mb-4 font-semibold text-stone-700 tablet:text-xl tablet-md:w-4/5 desktop-md:text-2xl">
+			<div className=" mb-8 justify-center tablet:flex tablet-md:h-[60vh]">
+				<section className="  py-4 px-2 leading-6 android-md/2:px-4  tablet:w-4/5 desktop:w-3/5 ">
+					<div className=" ">
+						<article className="my-10 mx-2 rounded border border-stone-300 p-6   tablet-md:w-4/5 laptop:w-[50rem]">
+							<p className="mb-4 font-semibold text-stone-700 tablet:text-xl tablet-md:w-4/5  desktop-md:text-3xl">
 								Signing up lets you track your progress, rewards, follow and
 								interacts with others in the community.
 							</p>
 
 							<div className=" flex items-center justify-between">
-								<span className=" inline-block  h-1 w-[55%] border-t-2 border-slate-400 pr-4 tablet-md:w-3/5"></span>
+								<span className=" inline-block  h-1 w-[55%] border-t-2 border-slate-400 pr-4 tablet-md:w-2/5"></span>
 								<Button path={'/signup'} type={'fill'}>
 									Sign up
 								</Button>
@@ -72,9 +75,9 @@ export default function HomePage() {
 						</article>
 					</div>
 
-					<div className="borderTest tablet:flex tablet:justify-end">
-						<article className="borderTest my-10 mx-2 pt-4 tablet:w-96 tablet-md:w-4/5 laptop:w-[40rem]">
-							<p className="mb-4 font-semibold text-stone-700 tablet:text-xl tablet-md:w-4/5 tablet-md:text-2xl">
+					<div className=" tablet:flex tablet:justify-end">
+						<article className="my-10 mx-2 rounded border border-stone-300 p-6   tablet-md:w-4/5 laptop:w-[50rem]">
+							<p className="mb-4 font-semibold text-stone-700 tablet:text-xl tablet-md:w-4/5  desktop-md:text-3xl">
 								Or you can start learning right away without saving your
 								progress.
 							</p>
